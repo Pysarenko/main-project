3f724736653d02c9a13cbc51ece3179d55e43050
Cath <pisarenko.ek@gmail.com>
2023-12-04 09:09:08 +0200

    Сommit Array Exercises - bug fixed

--- src/main/java/com/dev/mainproject/ArrayExercises.java
+++ src/main/java/com/dev/mainproject/ArrayExercises.java
@@ -18,12 +18,14 @@ public class ArrayExercises {
 
     public static int[][] sort(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length - 1; j++) {
-                 int k = arr[i][j];
-                 if (arr[i][j] > arr[i][j+1]) {
-                     arr[i][j] = arr[i][j+1];
-                     arr[i][j+1] = k;
-                 }
+            for (int l = 0; l < arr[i].length; l++) {
+                for (int j = 0; j < arr[i].length - 1; j++) {
+                    int k = arr[i][j];
+                    if (arr[i][j] > arr[i][j+1]) {
+                        arr[i][j] = arr[i][j+1];
+                        arr[i][j+1] = k;
+                    }
+                }
             }
         }
         return arr;
