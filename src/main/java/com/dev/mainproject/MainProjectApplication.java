fa1a8f239f98c3caf0867b211c6d3acc834a580e
Serhii Lukianenko <lukyanenko88@gmail.com>
2023-12-07 14:29:42 +0200

    Generics for arrayList

--- src/main/java/com/dev/mainproject/MainProjectApplication.java
+++ src/main/java/com/dev/mainproject/MainProjectApplication.java
@@ -3,9 +3,17 @@ package com.dev.mainproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 @SpringBootApplication
 public class MainProjectApplication {
     public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        List<String> linkedList= new LinkedList<>();
+
+
         SpringApplication.run(MainProjectApplication.class, args);
     }
 
